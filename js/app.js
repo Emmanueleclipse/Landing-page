ccb814223fae62af37c4fe1a048a79af68dbdeae
Emmannuel Sunday <sunasunday111@gmail.com>
2020-05-24 21:43:45 +0300

    Inital commit (any name )

--- js/app.js
+++ js/app.js
@@ -1,9 +1,45 @@
 ﻿$(document).ready(function () {
     $(document).ready(function () {
         $('.owl-carousel').owlCarousel({
-            items: 3,
-            autoplay: true,
-            nav:true
+            //autoplay: true,
+            responsiveClass: true,
+            responsive: {
+                0: {
+                    items: 1,
+                    nav: false,
+                    dots: true
+                },
+                600: {
+                    items: 2,
+                    nav: true,
+                    dots: false
+                },
+                900: {
+                    items: 3,
+                    nav: true,
+                    dots: false
+                }
+            }
         });
     });
+
+  
+
+    $('#collapsibleNavbar')
+        .on('shown.bs.collapse', function () {
+            $(".navbar-toggler").find("span").addClass("fa-times");
+            $(".navbar-toggler").find("span").removeClass("fa-bars");
+        })
+        .on('hidden.bs.collapse', function () {
+            $(".navbar-toggler").find("span").removeClass("fa-times");
+            $(".navbar-toggler").find("span").addClass("fa-bars");
+        });
+
+    $("#collapsibleNavbar .navbar-nav a").click(function () {
+        $(".navbar-toggler").find("span").removeClass("fa-times");
+        $(".navbar-toggler").find("span").addClass("fa-bars");
+        $(".navbar-collapse").removeClass("show");
+    })
+
+    var scroll = new SmoothScroll('a[href*="#"]');
 })
